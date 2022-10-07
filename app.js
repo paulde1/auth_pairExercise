321ce1ab03d45a3c8a4e69f34978795b081add25
Prince Karim <prikar79@gmail.com>
2022-10-07 15:48:31 -0400

    switch-off

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 const express = require('express');
 const app = express();
-const { models: { User }} = require('./db');
+const {
+  models: { User },
+} = require('./db');
 const path = require('path');
-// const token = require('jsonwebtoken')
-const dotenv = require('dotenv').config()
+const token = require('jsonwebtoken');
+const dotenv = require('dotenv').config();
 // middleware
 app.use(express.json());
-console.log("this is the env",process.env.JWT)
-// routes
-app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
 
-app.post('/api/auth', async(req, res, next)=> {
+// routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.post('/api/auth', async (req, res, next) => {
   try {
-    res.send({ token: await User.authenticate(req.body)});
-  }
-  catch(ex){
+    res.send({ token: await User.authenticate(req.body) });
+  } catch (ex) {
     next(ex);
   }
 });
 
-app.get('/api/auth', async(req, res, next)=> {
-    console.log(req.body)
+app.get('/api/auth', async (req, res, next) => {
   try {
     res.send(await User.byToken(req.headers.authorization));
-  }
-  catch(ex){
+  } catch (ex) {
     next(ex);
   }
 });
--- db.js
+++ db.js
@@ -1,85 +1,84 @@
 const Sequelize = require('sequelize');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 // const token = jwt.sign({ userId:  })
-
-
 
 const { STRING } = Sequelize;
 const config = {
-  logging: false
+  logging: false,
 };
 
-if(process.env.LOGGING){
+if (process.env.LOGGING) {
   delete config.logging;
 }
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db', config);
+const conn = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost/acme_db',
+  config
+);
 
 const User = conn.define('user', {
   username: STRING,
-  password: STRING
+  password: STRING,
 });
 
-User.byToken = async(token)=> {
+User.byToken = async (token) => {
   try {
-    console.log(token, "before")
+    const newToken = jwt.verify(token, process.env.JWT);
     const user = await User.findByPk(token);
-    if(user){
-        // console.log(token)
+    if (user) {
       return user;
     }
     const error = Error('bad credentials');
     error.status = 401;
-    // console.log(token, "from err")
+
     throw error;
-  }
-  catch(ex){
+  } catch (ex) {
     const error = Error('bad credentials');
-    error.status = 401;
-    // console.log(token, "from catch err")
+    error.status = 402;
+
     throw error;
   }
 };
 
-User.authenticate = async({ username, password })=> {
+User.authenticate = async ({ username, password }) => {
   const user = await User.findOne({
     where: {
       username,
-      password
-    }
+      password,
+    },
   });
-  if(user){
-    console.log(user)
-    const token = jwt.sign({ userId: user.id}, process.env.JWT)
-    console.log(token)
-    return token
+  if (user) {
+    const token = jwt.sign({ userId: user.id }, process.env.JWT);
+    console.log('here', token);
+    return token;
+    // return user.id;
   }
   const error = Error('bad credentials');
-  error.status = 401;
+  error.status = 403;
   throw error;
 };
 
-const syncAndSeed = async()=> {
+const syncAndSeed = async () => {
   await conn.sync({ force: true });
   const credentials = [
-    { username: 'lucy', password: 'lucy_pw'},
-    { username: 'moe', password: 'moe_pw'},
-    { username: 'larry', password: 'larry_pw'}
+    { username: 'lucy', password: 'lucy_pw' },
+    { username: 'moe', password: 'moe_pw' },
+    { username: 'larry', password: 'larry_pw' },
   ];
   const [lucy, moe, larry] = await Promise.all(
-    credentials.map( credential => User.create(credential))
+    credentials.map((credential) => User.create(credential))
   );
   return {
     users: {
       lucy,
       moe,
-      larry
-    }
+      larry,
+    },
   };
 };
 
 module.exports = {
   syncAndSeed,
   models: {
-    User
-  }
+    User,
+  },
 };
